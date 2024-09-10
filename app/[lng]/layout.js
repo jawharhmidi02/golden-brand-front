1abfb51e6a7cac8c0a411ba21481bd911ea09e56
Jawhar Hmidi <jawharhmidi02@gmail.com>
2024-09-10 10:39:57 +0100

    .

--- app/[lng]/layout.js
+++ app/[lng]/layout.js
@@ -1,9 +1,9 @@
 import "./globals.css";
 import "./animations.css";
-import "../assets/fontawesome/css/fontawesome.css";
-import "../assets/fontawesome/css/brands.css";
-import "../assets/fontawesome/css/solid.css";
-import "../assets/fontawesome/css/all.css";
+import "/public/assets/fontawesome/css/fontawesome.css";
+import "/public/assets/fontawesome/css/brands.css";
+import "/public/assets/fontawesome/css/solid.css";
+import "/public/assets/fontawesome/css/all.css";
 
 export const metadata = {
   title: "GoldenBrand: Home",
