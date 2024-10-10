d050b0d587695501c2eab60289aa53f17554cf03
Lafi Raed <browniesandmuffin@gmail.com>
2024-10-10 17:28:50 +0100

    Products page search something

--- app/[lng]/products/page.js
+++ app/[lng]/products/page.js
@@ -1,40 +1,84 @@
 "use client";
 
-import Image from "next/image";
+import CardPage from "@/components/Card/Card";
 import React, { useState } from "react";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 
 const ProductPage = ({ searchParams }) => {
   const searchQuery = searchParams.search || "";
   const [products, setProducts] = useState([
     {
       img: "/images/products/image1.png",
-      name: "name1",
-      desc: "description1",
+      name: "S. STEEL WORK TABLE WITHOUT UNDERSHELF",
+      desc: [
+        "1.0mmTHK., GR.304 #4 FINISH TOP PLATE, BACKSPLASH",
+        "1.0MMTHK., GR.304, #4 FINISH, STIFFENERS",
+        "40 x 40mm S. STEEL SQUARE TUBE LEG SUPPORT ON AJUSTABLE BULLET TYPE FEET",
+      ],
     },
     {
       img: "/images/products/image2.png",
-      name: "name2",
-      desc: "description2",
+      name: "S. STEEL MOBILE TABLE WITH TWO SHELF",
+      desc: [
+        "1.0mmTHK., GR.304, #4 FINISH TOP PLATE, STIFFENER",
+        "1.2mm THK., GR.304, #4 FINISH BASE PLATE",
+        "1.0mm THK., GR.304, #4 FINISH MID & UNDERSHELF",
+        "40 x 40mm S. STEEL SQUARE TUBE LEGS AND BRACING",
+        "4 RUBBER CASTER WHEEL, ALL SWIVEL, 2 WITH BRAKES",
+      ],
     },
     {
       img: "/images/products/image3.png",
-      name: "name3",
-      desc: "description3",
+      name: "S. STEEL SINGLE BOWL SINK TABLE",
+      desc: [
+        "1.2mmTHK., GR.304, #4 FINISH TOP PLATE, BACKSPLASH",
+        "1.0mmTHK., GR.304, #4 FINISH, STIFFENERS",
+        "40 x 40mm S. STEEL SQUARE TUBE FOR BRACE AND LEG SUPPORT WITH ADJUSTABLE BULLET TYPE FEET",
+        "500 x 500 x 300mm S. STEEL SINK",
+      ],
     },
     {
       img: "/images/products/image4.png",
-      name: "name4",
-      desc: "description4",
+      name: "S. STEEL BASE CABINET WITH 3 LAYER DRAWER",
+      desc: ["1.2mmTHK., GR.304, #4 FINISH TOP PLATE, BACKSPLASH, STIFFENERS"],
     },
   ]);
 
   return (
-    <div>
-      {products.map((item, index) => (
-        <Image key={index} src={item.img} width={100} height={100} />
-      ))}
+    <div className="flex flex-col justify-center gap-4 max-w-[58vw] mx-auto ">
+      <div className="flex flex-col gap-4">
+        {products?.map((product, index) => (
+          <CardPage key={index} product={product}/>
+        ))}
+      </div>
+      <div>
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious href="#" />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">1</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationNext href="#" />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </div>
     </div>
   );
-};  
+};
 
 export default ProductPage;
--- components/Card/Card.js
+++ components/Card/Card.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import { Separator } from "../ui/separator";
 
-const Card = () => {
+const CardPage = ({ product }) => {
   return (
-    <div>Card</div>
-  )
-}
+    <div className="flex bg-neutral-100 border-neutral-300 flex-row justify-start gap-5 p-[35px] border-[1px]">
+      <Image
+        src={product.img}
+        height={300}
+        width={300}
+        className="rounded-sm"
+      />
+      <div className="flex flex-col">
+        <span className="font-bold font-lato text-3xl mb-4 text-[var(--fifth-color-primary)]">
+          {product.name}
+        </span>
+        <Separator className="bg-neutral-300" />
+        <ul className="list-disc list-inside mt-3">
+          {product.desc.map((description, index) => (
+            <li className="font-lato mb-1" key={index}>
+              {description}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default CardPage;
