e7cd72e280dde9b5da92abb2a99d241699df217e
Jawhar Hmidi <jawharhmidi02@gmail.com>
2024-10-16 22:40:17 +0100

    ,

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
+    <div className="pt-20 ">
       <div className="services border-t-[1px] border-black p-10 flex items-center justify-center">
         <div className="service"></div>
         <div className="service"></div>
