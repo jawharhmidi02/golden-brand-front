316c2f3443325398145b61fea745ba1521fbe22a
Lafi Raed <browniesandmuffin@gmail.com>
2024-10-28 01:21:04 +0100

    .

--- components/nav/Nav.js
+++ components/nav/Nav.js
@@ -33,8 +33,8 @@ const Nav = ({ lng }) => {
       <div className="left">
         <Image
           src="/images/icon.png"
-          width={100}
-          height={100}
+          width={220}
+          height={0}
           alt="icon"
           loading="lazy"
           onClick={() => {
