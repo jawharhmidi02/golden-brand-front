34a257318a754035cb8d6d4c31dccfce7e4417bd
BrownieZzZzZz <browniesandmuffin@gmail.com>
2024-11-30 23:50:14 +0100

    .

--- app/admin/page.js
+++ app/admin/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>Dashboard pagee</div>
+    <div>Dashboard page</div>
   )
 }
 
--- app/admin/sign-in/page.js
+++ app/admin/sign-in/page.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>Sign In Page</div>
+    <div className="">Sign In Page</div>
   )
 }
 
 export default page
+
