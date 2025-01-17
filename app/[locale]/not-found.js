02ea7424f31ab93795be8292d3f7a369c5c04607
Jawhar Hmidi <jawharhmidi02@gmail.com>
2025-01-17 20:09:52 +0100

    asda

--- app/[locale]/not-found.js
+++ app/[locale]/not-found.js
@@ -1,26 +1,29 @@
 "use client";
-import { useRouter } from "next/navigation";
+
+import { useContext } from "react";
+
+import { UserAuthContext } from "@/contexts/AuthContext";
 
 export default function NotFound() {
-  const router = useRouter();
+  const { ChangeUrl } = useContext(UserAuthContext);
 
   return (
-    <section className="flex min-h-screen items-center bg-[#171821]">
+    <section className="flex min-h-screen items-center">
       <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
         <div className="mx-auto max-w-screen-sm text-center">
           <h1 className="mb-4 text-7xl font-extrabold tracking-tight text-[#059669] lg:text-9xl">
             404
           </h1>
-          <p className="mb-4 text-3xl font-bold tracking-tight text-[#90adc6] md:text-4xl">
+          <p className="mb-4 text-3xl font-bold tracking-tight text-[#059669] md:text-4xl">
             Page Not Found
           </p>
-          <p className="mb-4 text-lg font-light text-[#e9eaec]">
+          <p className="mb-4 text-lg">
             Sorry, we can't find the page you're looking for. Feel free to go
             back to the home page.
           </p>
           <div className="flex justify-center gap-3">
             <button
-              onClick={() => router.push("/")}
+              onClick={() => ChangeUrl("/")}
               className="my-4 inline-flex rounded-lg bg-[#059669] px-5 py-2.5 text-center text-sm font-medium text-white transition-colors hover:bg-[#064e3b] focus:outline-none focus:ring-4 focus:ring-[#064e3b]"
             >
               Go to Homepage
--- app/admin/not-found.js
+++ app/admin/not-found.js
@@ -1,11 +1,14 @@
 "use client";
-import { useRouter } from "next/navigation";
+
+import { useContext } from "react";
+
+import { AdminAuthContext } from "@/contexts/AuthContext";
 
 export default function NotFound() {
-  const router = useRouter();
+  const { ChangeUrl } = useContext(AdminAuthContext);
 
   return (
-    <section className="flex min-h-screen items-center bg-[#171821]">
+    <section className="flex min-h-screen w-full items-center bg-[#171821]">
       <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
         <div className="mx-auto max-w-screen-sm text-center">
           <h1 className="mb-4 text-7xl font-extrabold tracking-tight text-[#73cabe] lg:text-9xl">
@@ -14,13 +17,13 @@ export default function NotFound() {
           <p className="mb-4 text-3xl font-bold tracking-tight text-[#a9dfd8] md:text-4xl">
             Page Not Found
           </p>
-          <p className="mb-4 text-lg font-light text-[#4f5056]">
+          <p className="mb-4 text-lg font-light text-neutral-200">
             Sorry, we can't find the page you're looking for. Feel free to go
             back to the dashboard.
           </p>
           <div className="flex justify-center gap-3">
             <button
-              onClick={() => router.push("/admin")}
+              onClick={() => ChangeUrl("/admin/dashboard")}
               className="my-4 inline-flex rounded-lg bg-[#73cabe] px-5 py-2.5 text-center text-sm font-medium text-[#1c1d27] transition-colors hover:bg-[#a9dfd8] focus:outline-none focus:ring-4 focus:ring-[#a9dfd8]"
             >
               Back to Dashboard
