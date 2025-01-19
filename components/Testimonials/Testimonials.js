a9222a797585b733fe1f23c703df556b12b309c3
Jawhar Hmidi <jawharhmidi02@gmail.com>
2025-01-19 17:04:22 +0100

    asd

--- components/Testimonials/Testimonials.js
+++ components/Testimonials/Testimonials.js
@@ -11,6 +11,7 @@ import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 
 const Testimonials = () => {
   const tCommon = useTranslations("common");
@@ -119,6 +120,22 @@ const Testimonials = () => {
     ],
   };
 
+  const [slideHeight, setSlideHeight] = useState("");
+  const swiperContainerRef = useRef(null);
+
+  useEffect(() => {
+    if (swiperContainerRef.current) {
+      const slides =
+        swiperContainerRef.current.querySelectorAll(".swiper-slide");
+      let maxHeight = 0;
+
+      slides.forEach((slide) => {
+        maxHeight = Math.max(maxHeight, slide.scrollHeight);
+      });
+
+      setSlideHeight(maxHeight);
+    }
+  }, [swiperContainerRef]);
   return (
     <div className="py-12" dir={isRTL ? "rtl" : "ltr"}>
       <div className="mx-auto max-w-screen-xl">
@@ -164,54 +181,63 @@ const Testimonials = () => {
               slidesPerView: 3,
             },
           }}
-          className="max-w-[95dvw]"
+          ref={swiperContainerRef}
+          className="h-auto max-w-[95dvw]"
         >
-          {testimonials[locale].map((testimonial, index) => (
-            <SwiperSlide key={index}>
-              <div className="slide_active:border-amber-600 group mx-auto flex flex-col justify-between rounded-xl border border-solid border-gray-300 bg-white p-6 transition-all duration-500 hover:border-amber-600 hover:shadow-sm">
-                <div>
-                  <div className="mb-7 flex items-center gap-2 text-amber-500 transition-all duration-500">
-                    <svg
-                      className="h-5 w-5"
-                      viewBox="0 0 18 17"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M8.10326 1.31699C8.47008 0.57374 9.52992 0.57374 9.89674 1.31699L11.7063 4.98347C11.8519 5.27862 12.1335 5.48319 12.4592 5.53051L16.5054 6.11846C17.3256 6.23765 17.6531 7.24562 17.0596 7.82416L14.1318 10.6781C13.8961 10.9079 13.7885 11.2389 13.8442 11.5632L14.5353 15.5931C14.6754 16.41 13.818 17.033 13.0844 16.6473L9.46534 14.7446C9.17402 14.5915 8.82598 14.5915 8.53466 14.7446L4.91562 16.6473C4.18199 17.033 3.32456 16.41 3.46467 15.5931L4.15585 11.5632C4.21148 11.2389 4.10393 10.9079 3.86825 10.6781L0.940384 7.82416C0.346867 7.24562 0.674378 6.23765 1.4946 6.11846L5.54081 5.53051C5.86652 5.48319 6.14808 5.27862 6.29374 4.98347L8.10326 1.31699Z"
-                        fill="currentColor"
+          <div className="swiper-wrapper">
+            {testimonials[locale].map((testimonial, index) => (
+              <SwiperSlide
+                key={index}
+                className="flex-grow"
+                style={{
+                  height: slideHeight ? `${slideHeight - 10}px` : "auto",
+                }}
+              >
+                <div className="group flex h-full flex-col justify-between rounded-xl border border-solid border-gray-300 bg-white p-6 transition-all duration-500 hover:border-amber-600 hover:shadow-sm">
+                  <div className="flex flex-1 flex-col">
+                    <div className="mb-7 flex items-center gap-2 text-amber-500 transition-all duration-500">
+                      <svg
+                        className="h-5 w-5"
+                        viewBox="0 0 18 17"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M8.10326 1.31699C8.47008 0.57374 9.52992 0.57374 9.89674 1.31699L11.7063 4.98347C11.8519 5.27862 12.1335 5.48319 12.4592 5.53051L16.5054 6.11846C17.3256 6.23765 17.6531 7.24562 17.0596 7.82416L14.1318 10.6781C13.8961 10.9079 13.7885 11.2389 13.8442 11.5632L14.5353 15.5931C14.6754 16.41 13.818 17.033 13.0844 16.6473L9.46534 14.7446C9.17402 14.5915 8.82598 14.5915 8.53466 14.7446L4.91562 16.6473C4.18199 17.033 3.32456 16.41 3.46467 15.5931L4.15585 11.5632C4.21148 11.2389 4.10393 10.9079 3.86825 10.6781L0.940384 7.82416C0.346867 7.24562 0.674378 6.23765 1.4946 6.11846L5.54081 5.53051C5.86652 5.48319 6.14808 5.27862 6.29374 4.98347L8.10326 1.31699Z"
+                          fill="currentColor"
+                        />
+                      </svg>
+                      <span className="text-base font-semibold text-amber-600">
+                        {testimonial.rate}
+                      </span>
+                    </div>
+                    <p className="flex-grow text-base leading-6 text-gray-600 transition-all duration-500 group-hover:text-gray-800">
+                      {testimonial.message}
+                    </p>
+                  </div>
+                  <div className="mt-6 flex items-center gap-5 border-t border-solid border-gray-200 pt-5">
+                    <div className="relative h-10 w-10">
+                      <Image
+                        className="rounded-full object-cover"
+                        src={testimonial.img}
+                        alt={`${testimonial.name}'s avatar`}
+                        fill
+                        sizes="(max-width: 40px) 100vw"
                       />
-                    </svg>
-                    <span className="text-base font-semibold text-amber-600">
-                      {testimonial.rate}
-                    </span>
-                  </div>
-                  <p className="slide_active:text-gray-800 pb-8 text-base leading-6 text-gray-600 transition-all duration-500 group-hover:text-gray-800">
-                    {testimonial.message}
-                  </p>
-                </div>
-                <div className="mt-auto flex items-center gap-5 border-t border-solid border-gray-200 pt-5">
-                  <div className="relative h-10 w-10">
-                    <Image
-                      className="rounded-full object-cover"
-                      src={testimonial.img}
-                      alt={`${testimonial.name}'s avatar`}
-                      fill
-                      sizes="(max-width: 40px) 100vw"
-                    />
-                  </div>
-                  <div className="block">
-                    <h5 className="mb-1 font-medium text-gray-900 transition-all duration-500">
-                      {testimonial.name}
-                    </h5>
-                    <span className="text-sm leading-4 text-gray-500">
-                      {testimonial.job}
-                    </span>
+                    </div>
+                    <div className="block">
+                      <h5 className="mb-1 font-medium text-gray-900 transition-all duration-500">
+                        {testimonial.name}
+                      </h5>
+                      <span className="text-sm leading-4 text-gray-500">
+                        {testimonial.job}
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </SwiperSlide>
-          ))}
+              </SwiperSlide>
+            ))}
+          </div>
         </Swiper>
       </div>
     </div>
